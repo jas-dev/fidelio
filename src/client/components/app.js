2c12ec991359b63c302634e1152f2c730f9eb637
jas-dev <jaysamora@outlook.com>
2019-07-23 23:59:55 -0700

    added basic frontend api call to get questions

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import '../assets/css/app.scss';
 import AddQuestion from './addQuestion'
+import StudentTable from "../../../../frontend/react_sgt/src/components/app";
 
-const App = () => (
-    <div>
-        <div className="app">
+class App extends Component {
+    state = {
+        questions: [],
+        error: ''
+    };
 
-            <h1>Welcome to fidelio</h1>
+    async getStudentData(){
+        try{
+            const response = await axios.get('/api/questions');
+            this.setState({
+                questions: response.data.data
+            });
+        }catch(err){
+            this.setState({
+                error: 'Error retrieving student data'
+            });
+        }
+    };
+    render(){
+        return (
             <div>
-                <AddQuestion/>
-            </div>
+                <h1 className="center">fidelio</h1>
+                <h5 className='red-text text-darken-2'>{this.state.error}</h5>
+                <div className="row">
 
-        </div>
-    </div>
-);
+                </div>
+            </div>
+        );
+    };
+}
 
 export default App;
